c82001623c1e6c778e579bfc1f4085918213cd4d
rachelblr19 <rachelblr19@gmail.com>
2026-07-16 21:47:30 -0400

    morew

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rachelbl19.github.io',
-  base: '/devportfolio',
-  },
-);
+site: "https://{your_github_username}.github.io",
+base: "/{forked_repo_name}/",
+vite: {
+plugins: [tailwindcss()],
+},
+});
