59fcd54ab3521af03723da4ae354b5807d8fb64b
rachelblr19 <rachelblr19@gmail.com>
2026-07-16 21:42:40 -0400

    Add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+
+
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
+  site: 'https://rachelbl19.github.io',
+  base: '/devportfolio',
   },
-});
+);
